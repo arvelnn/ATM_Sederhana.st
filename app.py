15ce9c5cc9f8995f0393b7d11eb76a0c40b99803
zhalyyaa16 <arianizhafira06@gmail.com>
2026-06-21 15:59:11 +0700

    Mengubah fitur app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from login import login
 from data import akun
-from cek_saldo import tampilkan_saldo
 
 if "login" not in st.session_state:
     st.session_state.login = False
@@ -24,12 +23,3 @@ if st.button("Login"):
         st.success(f"Login berhasil! Selamat datang, {akun[user]['nama']}!")
     else:
         st.error("Nomor rekening atau PIN salah. Silakan coba lagi.")
-
-if st.session_state.login:
-
-    tampilkan_saldo(st.session_state.user)
-
-    if st.button("Logout"):
-        st.session_state.login = False
-        st.session_state.user = None
-        st.rerun()
